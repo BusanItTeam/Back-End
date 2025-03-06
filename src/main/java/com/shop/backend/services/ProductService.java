49454bfdefa673e4822d8673aaf7961f9f67c756
dh <dohyun134@naver.com>
2025-03-06 12:47:27 +0900

    메시지 아무거나~

--- src/main/java/com/shop/backend/services/ProductService.java
+++ src/main/java/com/shop/backend/services/ProductService.java
@@ -8,6 +8,9 @@ import com.shop.backend.repository.ProductAddImageRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
